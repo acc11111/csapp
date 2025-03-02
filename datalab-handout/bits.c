f889416b102edf67e09cb3f50340f23105ec9d4e
Acc <Acc@wsl.com>
2025-03-02 23:09:36 +0800

    finish some exercise in data-lab, there are note and some exercise left to finish

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -1,8 +1,8 @@
-/* 
- * CS:APP Data Lab 
- * 
+/*
+ * CS:APP Data Lab
+ *
  * <Please put your name and userid here>
- * 
+ *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
  *
@@ -10,7 +10,7 @@
  * compiler. You can still use printf for debugging without including
  * <stdio.h>, although you might get a compiler warning. In general,
  * it's not good practice to ignore compiler warnings, but in this
- * case it's OK.  
+ * case it's OK.
  */
 
 #if 0
@@ -132,42 +132,51 @@ NOTES:
  *      the correct answers.
  */
 
-
 #endif
-//1
-/* 
- * bitXor - x^y using only ~ and & 
+// 1
+/*
+ * bitXor - x^y using only ~ and &
  *   Example: bitXor(4, 5) = 1
  *   Legal ops: ~ &
  *   Max ops: 14
  *   Rating: 1
  */
-int bitXor(int x, int y) {
-  return 2;
+int bitXor(int x, int y)
+{
+  // 使用离散数学表达式转换即可
+  return ~((~(x & ~y)) & (~(~x & y)));
 }
-/* 
- * tmin - return minimum two's complement integer 
+/*
+ * tmin - return minimum two's complement integer
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 4
  *   Rating: 1
  */
-int tmin(void) {
-
-  return 2;
-
+int tmin(void)
+{
+  // 1-->0000...001
+  // 将0000... 001左移31位即可获得1000...000也就是tmin
+  int x = 1;
+  return x << 31;
 }
-//2
+// 2
 /*
  * isTmax - returns 1 if x is the maximum, two's complement number,
- *     and 0 otherwise 
+ *     and 0 otherwise
  *   Legal ops: ! ~ & ^ | +
  *   Max ops: 10
  *   Rating: 1
  */
-int isTmax(int x) {
-  return 2;
+int isTmax(int x)
+{
+  // tmax=0111...111
+  // tmax+1=1000...000
+  // 检查第一位为0，其余位为1
+  // 左侧括号判断x加一是否为互补状态，这里筛选出来的只有1111...111和0111...111是符合的
+  // 右侧括号判断x不是1111...111
+  return ((!((x + 1) ^ (~x))) & (!!(~x)));
 }
-/* 
+/*
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
  *   where bits are numbered from 0 (least significant) to 31 (most significant)
  *   Examples allOddBits(0xFFFFFFFD) = 0, allOddBits(0xAAAAAAAA) = 1
@@ -175,21 +184,32 @@ int isTmax(int x) {
  *   Max ops: 12
  *   Rating: 2
  */
-int allOddBits(int x) {
-  return 2;
+int allOddBits(int x)
+{
+  // 构造的数字必须在0-255之间，也就是0000 0000 0000 0000 0000 0000 1111 1111
+  // 因此通过移位和相加构造出目标1010101010...101010
+  // 通过取与运算获得原数字x的全部奇数为为原来的数字，偶数位0
+  // 在将这个过滤后仅剩奇数位数字的新数与奇数位全为1的进行取异或运算
+  // 如果结果为0则说明x的奇数位全为1，否则只要原来x中的奇数位有一个0就会出现结果为1
+  int mask = 0xAA;
+  mask = (mask << 8) + 0xAA;
+  mask = (mask << 8) + 0xAA;
+  mask = (mask << 8) + 0xAA;
+  return !((x & mask) ^ mask);
 }
-/* 
- * negate - return -x 
+/*
+ * negate - return -x
  *   Example: negate(1) = -1.
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 5
  *   Rating: 2
  */
-int negate(int x) {
-  return 2;
+int negate(int x)
+{
+  return (~x + 1);
 }
-//3
-/* 
+// 3
+/*
  * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
  *   Example: isAsciiDigit(0x35) = 1.
  *            isAsciiDigit(0x3a) = 0.
@@ -198,40 +218,62 @@ int negate(int x) {
  *   Max ops: 15
  *   Rating: 3
  */
-int isAsciiDigit(int x) {
-  return 2;
+int isAsciiDigit(int x)
+{
+  // 0x30 <= x <= 0x39将表达式变形一下
+  // 0x30 - x <= 0 <= 0x39 - x
+  // 0x2f - x就应该是一个负数，也就是1.....
+  // 0x39 - x就应该是一个正数，也就是0.....
+  // 左侧检测出来是tmin和大于0x2f的数
+  // 右侧检测出来的是小于等于0x39的数
+  // 但是这里有个坑点，tmin转换之后还是tmin，导致tmin满足第一条
+  // 同时第二条使用!导致检测出来的负数也满足非0所以使用!之后导致为真使得陷入了误区
+  // 这里其实就是检测那个1和检测那个0，然后1可以使用&检测，0可以使用^检测
+  return (((0x2f + (~x + 1)) >> 31 & 1) & (((0x39 + (~x + 1)) >> 31) ^ 1));
 }
-/* 
- * conditional - same as x ? y : z 
+/*
+ * conditional - same as x ? y : z
  *   Example: conditional(2,4,5) = 4
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 16
  *   Rating: 3
  */
-int conditional(int x, int y, int z) {
-  return 2;
+int conditional(int x, int y, int z)
+{
+  // x == 0 >> return z;
+  // x != 0 >> return y;
+  // 检查这个x是否为0
+  // !这是一个好东西，可以将所有的非0数通过!!变成1，然后通过-1也就是(~1 + 1)来实现为0
+  // 同时非0的数字可以使用一次!将其变成0，然后通过-1也就是(~1 + 1)来实现为-1也就是全1，此时使用&操作即可获得原来的数因为已经是全1
+  return ((!x + (~1 + 1)) & y) + ((!!x + (~1 + 1)) & z);
 }
-/* 
- * isLessOrEqual - if x <= y  then return 1, else return 0 
+/*
+ * isLessOrEqual - if x <= y  then return 1, else return 0
  *   Example: isLessOrEqual(4,5) = 1.
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 24
  *   Rating: 3
  */
-int isLessOrEqual(int x, int y) {
-  return 2;
+int isLessOrEqual(int x, int y)
+{
+  // 根据之前的0x30-0x39的思路一下子就可以将这个写出来
+  // 两者做差取符号位
+  // 但是这里需要注意的是如果是<0才是稳定获得1,如果是<=0那就是可能为1也可能为0,所以做好使用非负的符号位为0
+  return !(y + (~x + 1) >> 31 & 1);
 }
-//4
-/* 
- * logicalNeg - implement the ! operator, using all of 
+// 4
+/*
+ * logicalNeg - implement the ! operator, using all of
  *              the legal operators except !
  *   Examples: logicalNeg(3) = 0, logicalNeg(0) = 1
  *   Legal ops: ~ & ^ | + << >>
  *   Max ops: 12
- *   Rating: 4 
+ *   Rating: 4
  */
-int logicalNeg(int x) {
-  return 2;
+int logicalNeg(int x)
+{
+  //
+  return ((x | (~x + 1)) >> 31) + 1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -245,11 +287,16 @@ int logicalNeg(int x) {
  *  Max ops: 90
  *  Rating: 4
  */
-int howManyBits(int x) {
+int howManyBits(int x)
+{
+  int sign = x >> 31;
+  int abs_x = ((!sign + (~1 + 1)) & (~x + 1)) + (!!sign + (~1 + 1) & x);
+  int bits = 0;
+  bits = bits + (abs_x >> 1);
   return 0;
 }
-//float
-/* 
+// float
+/*
  * floatScale2 - Return bit-level equivalent of expression 2*f for
  *   floating point argument f.
  *   Both the argument and result are passed as unsigned int's, but
@@ -260,10 +307,11 @@ int howManyBits(int x) {
  *   Max ops: 30
  *   Rating: 4
  */
-unsigned floatScale2(unsigned uf) {
+unsigned floatScale2(unsigned uf)
+{
   return 2;
 }
-/* 
+/*
  * floatFloat2Int - Return bit-level equivalent of expression (int) f
  *   for floating point argument f.
  *   Argument is passed as unsigned int, but
@@ -275,10 +323,11 @@ unsigned floatScale2(unsigned uf) {
  *   Max ops: 30
  *   Rating: 4
  */
-int floatFloat2Int(unsigned uf) {
+int floatFloat2Int(unsigned uf)
+{
   return 2;
 }
-/* 
+/*
  * floatPower2 - Return bit-level equivalent of the expression 2.0^x
  *   (2.0 raised to the power x) for any 32-bit integer x.
  *
@@ -286,11 +335,12 @@ int floatFloat2Int(unsigned uf) {
  *   representation as the single-precision floating-point number 2.0^x.
  *   If the result is too small to be represented as a denorm, return
  *   0. If too large, return +INF.
- * 
- *   Legal ops: Any integer/unsigned operations incl. ||, &&. Also if, while 
- *   Max ops: 30 
+ *
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. Also if, while
+ *   Max ops: 30
  *   Rating: 4
  */
-unsigned floatPower2(int x) {
-    return 2;
+unsigned floatPower2(int x)
+{
+  return 2;
 }
