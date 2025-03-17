437e9f649aee2c2b91ae0cbebab0eeeabb176297
Acc <Acc@wsl.com>
2025-03-17 16:34:49 +0800

    add v8 note

--- bomb/tmp.c
+++ bomb/tmp.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-int func4(int a, int b, int num1, int num2) {
-    int temp = num1;
+int func4(int num1, int b, int c, int num2) {
+    int temp = c;
     temp = temp - b;
     num2 = num2 - temp;
     num2 >>= 31;
     temp = temp + num2;
     num2 = temp + b;
-    if (a <= num2) {
+    if (num1 <= num2) {
         temp = 0;
-        if (a >= num2) {
+        if (num1 >= num2) {
             temp = 1;
         } else {
             b = num2 + 1;
-            temp = func4(a, b, num1, num2);
+            temp = func4(num1, b, c, num2);
         }
     } else {
-        a = num2 - 1;
-        temp = func4(a, b, num1, num2);
+        num1 = num2 - 1;
+        temp = func4(num1, b, c, num2);
         temp = temp + temp;
         return temp;
     }
